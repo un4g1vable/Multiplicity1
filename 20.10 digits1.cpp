35c59365b6b83b8fb02bd2fac9b0359237a844ef
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-20 01:27:19 +0500

    Add files via upload

---
+++ 20.10 digits1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string> 
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+    static int counter;
+    int first_digit;
+    cout << "Введите число: ";
+    cin >> first_digit;
+    int second_digit = first_digit;
+    while (second_digit > 0)
+    {
+        if ((second_digit % 10) != 0 and first_digit % (second_digit % 10) == 0)
+        {
+            counter++;
+            cout << first_digit << " делить на " << (second_digit % 10) << " = " << first_digit / (second_digit % 10) << endl;
+            second_digit = second_digit / 10;
+        }
+        else
+            second_digit = second_digit / 10;
+
+    }
+    if (counter > 0)
+        cout << "Число делится как минимум на одну из своих цифр";
+    else
+        cout << "Число не делится ни на одну из своих цифр";
+
+
+}
+   
